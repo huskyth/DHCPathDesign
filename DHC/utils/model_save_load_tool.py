d273f0049d69e1e7fd63f77bc6e00200de7e42c5
huskyth <1622313177@qq.com>
2024-03-28 14:47:33 +0800

    feat: model rm

--- DHC/utils/model_save_load_tool.py
+++ DHC/utils/model_save_load_tool.py
@@ -1,13 +1,10 @@
 import torch
 
-RESUME_MODEL_NAME = ''
 TEST_MODEL_NAME = '2022-10-26-18-12000-0.86.pth'
 
 
-def model_save(model, path, counter, avg_loss, avg_reward, avg_finish_cases, avg_step):
-    state = {'model_state': model.state_dict(), 'counter': counter, 'avg_loss': avg_loss,
-             'avg_reward': avg_reward, 'avg_finish_cases': avg_finish_cases, 'avg_step': avg_step}
-    torch.save(state, path)
+def model_save(model, path):
+    torch.save(model, path)
 
 
 def model_load(path, device):
