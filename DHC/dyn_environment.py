f0e500a97a55189797992ff4997425b254f64abb
huskyth <1622313177@qq.com>
2024-03-28 11:20:34 +0800

    feat: code clean

--- DHC/dyn_environment.py
+++ DHC/dyn_environment.py
@@ -1,10 +1,8 @@
-import random
-from typing import List, Union
+from typing import List
 import numpy as np
 import matplotlib.pyplot as plt
 
 plt.ion()
-from matplotlib import colors
 import configs
 from construct_map.static_map import StaticObstacle
 from construct_map.dynamic_map import DynamicPedestrian
@@ -30,14 +28,11 @@ color_map = np.array([[255, 255, 255],  # white
                       [0, 250, 154]])  # green
 
 
-# print("dynamic map")
-
 def map_partition(map):
     """
     partitioning map into independent partitions
     把可行区域提取出来
     """
-    # 值为0的数组的索引；即把free区域的位置坐标取出来
     empty_pos = np.argwhere(map == 0).astype(np.int).tolist()
 
     empty_pos = [tuple(pos) for pos in empty_pos]
@@ -84,59 +79,38 @@ def map_partition(map):
     return partition_list
 
 
-from utils.coordinate_tool import *
 from utils.math_tool import *
+
 
 class Environment:
     def __init__(self, num_agents: int = configs.init_env_settings[0], map_size: tuple = configs.map_size,
                  map_length: int = configs.init_env_settings[1],
                  obs_radius: int = configs.obs_radius, reward_fn: dict = configs.reward_fn, fix_density=None,
                  curriculum=False, init_env_settings_set=configs.init_env_settings):
+        self.heuri_map = None
+        self.fig = None
+        self.texts = None
+        self.imgs = None
         self.dyn_map = 0
         self.curriculum = curriculum
         if curriculum:
             self.env_set = [init_env_settings_set]
             self.num_agents = init_env_settings_set[0]
-            # self.map_size = (init_env_settings_set[1], init_env_settings_set[1])
             self.map_size = map_size
         else:
             self.num_agents = num_agents
-            # self.map_size = (map_length, map_length)
             self.map_size = map_size
 
-        ''''地图设置，根据unity导出的数据构建静态地图'''
-        # # set as same as in PRIMAL 设置
-        # 根据给定的障碍密度和地图大小生成地图
-        # if fix_density is None:
-        #     self.fix_density = False
-        #     self.obstacle_density = np.random.triangular(0, 0.33, 0.5)
-        # else:
-        #     self.fix_density = True
-        #     self.obstacle_density = fix_density
-        # # 从0-1序列中按照概率p采样map_size个数据；0代表free区域，1代表obstacle区域
-        # self.map = np.random.choice(2, self.map_size, p=[1-self.obstacle_density, self.obstacle_density]).astype(np.int)
-
-        # 实例化静态地图类
         self.static_obs = StaticObstacle()
-        self.map = self.static_obs.static_map  # 静态地图矩阵
-        # 实例化动态地图类，step加入动态地图
+        self.map = self.static_obs.static_map
         self.dynamic_ped = DynamicPedestrian(self.static_obs.rows, self.static_obs.columns)
-        # 记录当前时刻
         self.t = 0
 
         partition_list = map_partition(self.map)
         partition_list = [partition for partition in partition_list if len(partition) >= 2]
 
-        # 没有可行区域了，重新根据density等参数生成地图
-        # while len(partition_list) == 0:
-        #     self.map = np.random.choice(2, self.map_size, p=[1-self.obstacle_density, self.obstacle_density]).astype(np.int)
-        #     partition_list = map_partition(self.map)
-        #     partition_list = [ partition for partition in partition_list if len(partition) >= 2 ]
-
-        # 创建agent初始位置和目标位置数组
         self.agents_pos = np.empty((self.num_agents, 2), dtype=np.int)
         self.goals_pos = np.empty((self.num_agents, 2), dtype=np.int)
-        # 可行区域的数量
         pos_num = sum([len(partition) for partition in partition_list])
 
         # loop to assign agent original position and goal position for each agent
@@ -155,9 +129,7 @@ class Environment:
             partition_list[partition_idx].remove(pos)
             self.agents_pos[i] = np.asarray(pos, dtype=np.int)
 
-            # 打印初始位置
             x, y, is_in = grid2coord(self.agents_pos[i])
-            # print('agent id = {}, start position = {}, is in Plane = {}'.format(i, [x, y], is_in))
             if not is_in:
                 assert is_in, 'agent id = {}, start position = {}, is in Plane = {}'.format(i, [x, y], is_in)
             pos = random.choice(partition_list[partition_idx])
@@ -170,9 +142,8 @@ class Environment:
         self.obs_radius = obs_radius
 
         self.reward_fn = reward_fn
-        self.get_heuri_map()  # 静态地图
+        self.get_heuri_map()
         self.steps = 0
-        # 5应该是action空间
         self.last_actions = np.zeros((self.num_agents, 5, 2 * obs_radius + 1, 2 * obs_radius + 1), dtype=np.bool)
 
     def update_env_settings_set(self, new_env_settings_set):
@@ -183,28 +154,16 @@ class Environment:
         if self.curriculum:
             rand = random.choice(self.env_set)
             self.num_agents = rand[0]
-            # self.map_size = (rand[1], rand[1])
-            self.map_size = map_size  # 使用定义好的 mapsize
+            self.map_size = map_size
 
         elif num_agents is not None and map_length is not None:
             self.num_agents = num_agents
-            # self.map_size = (map_length, map_length)
             self.map_size = map_size
 
-        # 设定密度
-        # if not self.fix_density:
-        #     self.obstacle_density = np.random.triangular(0, 0.33, 0.5)
-
-        # self.map = np.random.choice(2, self.map_size, p=[1-self.obstacle_density, self.obstacle_density]).astype(np.float32)
-
         ''''地图设置，根据unity导出的数据构建静态地图'''
-        # set as same as in PRIMAL 设置
-        # 实例化静态地图类
         self.static_obs = StaticObstacle()
-        self.map = self.static_obs.static_map  # 静态地图矩阵
-        # 实例化动态地图类
+        self.map = self.static_obs.static_map
         self.dynamic_ped = DynamicPedestrian(self.static_obs.rows, self.static_obs.columns)
-        # 记录当前时刻
         self.t = 1
 
         partition_list = map_partition(self.map)
@@ -245,7 +204,6 @@ class Environment:
 
         return self.observe()
 
-    # 根据参数加载模型
     def load(self, map: np.ndarray, agents_pos: np.ndarray, goals_pos: np.ndarray):
 
         self.map = np.copy(map)
@@ -327,7 +285,6 @@ class Environment:
         self.heuri_map = np.pad(self.heuri_map, (
             (0, 0), (0, 0), (self.obs_radius, self.obs_radius), (self.obs_radius, self.obs_radius)))
 
-    # reward要受到动态行人影响
     def step(self, actions: List[int], pde_df=0):
         '''
         actions:
@@ -342,7 +299,7 @@ class Environment:
         self.dyn_map = pde_df
         assert len(actions) == self.num_agents, 'only {} actions as input while {} agents in environment'.format(
             len(actions), self.num_agents)
-        assert all([action_idx < 5 and action_idx >= 0 for action_idx in actions]), 'action index out of range'
+        assert all([5 > action_idx >= 0 for action_idx in actions]), 'action index out of range'
 
         checking_list = [i for i in range(self.num_agents)]
 
@@ -363,26 +320,19 @@ class Environment:
 
                 checking_list.remove(agent_id)
             else:
-                # move 这个地方，超出
                 origin_pos = [x for x in next_pos[agent_id]]
                 next_pos[agent_id] += action_list[actions[agent_id]]
-                # 添加朝着目标前进的奖赏为正，相反为负
                 move_reward_sign = 1 if is_next_position_toward_goal_by_distance(*origin_pos, *next_pos[agent_id],
                                                                                  *list(
                                                                                      self.goals_pos[agent_id])) else -1
-                rewards.append(abs(self.reward_fn['collision']) * move_reward_sign * rank_of_agents[agent_id] / len(self.agents_pos))
+                rewards.append(abs(self.reward_fn['collision']) * move_reward_sign * rank_of_agents[agent_id] / len(
+                    self.agents_pos))
                 content = 'agent {} reward = {}, rank = {}'.format(agent_id, rewards[-1], rank_of_agents[agent_id])
                 write_file(content, 'rank_reward.txt')
-                # print('start = {}, next = {}, goal = {}, reward_sign = {}, and reward = {}'.format(origin_pos,
-                #                                                                                    next_pos[agent_id],
-                #                                                                                    self.goals_pos[
-                #                                                                                        agent_id],
-                #                                                                                    move_reward_sign,
-                #                                                                                    rewards[-1]))
+
         # first round check, these two conflicts have the heightest priority
         for agent_id in checking_list.copy():
 
-            # 超出地图限制
             if np.any(next_pos[agent_id] < 0) or (next_pos[agent_id][0] >= self.map_size[0]) or (
                     next_pos[agent_id][1] >= self.map_size[1]):
                 # if np.any(next_pos[agent_id] < 0) or np.any(next_pos[agent_id] >= self.map_size[0]):
@@ -390,7 +340,6 @@ class Environment:
                 rewards[agent_id] = self.reward_fn['collision']
                 next_pos[agent_id] = self.agents_pos[agent_id]
                 checking_list.remove(agent_id)
-            # 碰到障碍物
             elif self.map[tuple(next_pos[agent_id])] == 1:
                 # collide obstacle
                 rewards[agent_id] = self.reward_fn['collision']
@@ -427,7 +376,6 @@ class Environment:
                         break
                 else:
                     content = 'len(target_agent_id) != 1 and len(target_agent_id) == {}'.format(len(target_agent_id))
-                    # write_file(content)
 
         # third round check, agent collision conflict
         no_conflict = False
@@ -455,8 +403,6 @@ class Environment:
 
                         collide_agent_id.remove(collide_agent_pos[0][2])
 
-                        # checking_list.remove(collide_agent_pos[0][2])
-
                     next_pos[collide_agent_id] = self.agents_pos[collide_agent_id]
                     for id in collide_agent_id:
                         rewards[id] = self.reward_fn['collision']
@@ -467,7 +413,6 @@ class Environment:
                     no_conflict = False
                     break
 
-        # self.history.append(np.copy(next_pos))
         self.agents_pos = np.copy(next_pos)
 
         self.steps += 1
@@ -518,23 +463,16 @@ class Environment:
         pos: used for caculating communication mask
 
         """
-        # 合并动态地图
-        dynamic_ped_map = 0
         if isinstance(self.dyn_map, pd.DataFrame) and self.dyn_map:  # 数据类型判断
             dynamic_ped_map = self.dyn_map
         else:
-            # 获取行人x y 坐标
             coorlist = self.dynamic_ped.pde_df.iloc[self.t].tolist()
-            # 得到栅格化中的位置
             dynamic_ped_map = self.dynamic_ped.get_pedcoor(coorlist)
-            self.t = self.t + 1  # 时间加一
+            self.t = self.t + 1
             # todo 只是测试，之后删掉
             if self.t >= len(self.dynamic_ped.pde_df):
                 self.t = 0
-        # 静态地图和动态地图合并
-        # self.map = self.static_obs.static_map
         self.map = self.static_obs.static_map + dynamic_ped_map
-        # 将大于1的全部置位1
         self.map[np.where(self.map >= 1)] = 1
 
         obs = np.zeros((self.num_agents, 6, 2 * self.obs_radius + 1, 2 * self.obs_radius + 1), dtype=np.bool)
@@ -554,7 +492,6 @@ class Environment:
             obs[i, 1] = obstacle_map[x:x + 2 * self.obs_radius + 1, y:y + 2 * self.obs_radius + 1]
             obs[i, 2:] = self.heuri_map[i, :, x:x + 2 * self.obs_radius + 1, y:y + 2 * self.obs_radius + 1]
 
-        # obs = np.concatenate((obs, self.last_actions), axis=1)
         return obs, np.copy(self.agents_pos)
 
     def render(self):
@@ -570,7 +507,6 @@ class Environment:
                 map[tuple(self.goals_pos[agent_id])] = 3
 
         map = map.astype(np.uint8)
-        # plt.xlabel('step: {}'.format(self.steps))
 
         # add text in plot
         self.imgs.append([])
@@ -588,7 +524,6 @@ class Environment:
         plt.imshow(color_map[map], animated=True)
 
         plt.show()
-        # plt.ion()
         plt.pause(0.5)
 
     def close(self, save=False):
